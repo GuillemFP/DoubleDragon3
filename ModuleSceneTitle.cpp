307f11a835ed52ac66f6fe0da48e28272910c07c
GuillemFP <guillemferre@gmail.com>
2016-12-29 21:56:24 +0100

    Added Title Scene

--- ModuleSceneTitle.cpp
+++ ModuleSceneTitle.cpp
@@ -1,16 +1,22 @@
 #include "Application.h"
 #include "ModuleInput.h"
+#include "ModuleWindow.h"
 #include "ModuleTextures.h"
 #include "ModuleRender.h"
+#include "ModuleAudio.h"
 #include "ModuleFadeToBlack.h"
 #include "ModuleSceneTitle.h"
 #include "Animation.h"
 #include "SDL/include/SDL_scancode.h"
 #include "JsonHandler.h"
 
-ModuleSceneTitle::ModuleSceneTitle(JSONParser* parser, bool active) : Module(MODULESCENE_TITLE, active)
+ModuleSceneTitle::ModuleSceneTitle(JSONParser* parser, bool active) : Module(MODULESCENE_TITLE, active), current_state(LOGO1)
 {
-	animated_title.LoadAnimation(parser, MODULESCENE_FIRST_ANIMATION);
+	if (parser->LoadObject(MODULESCENE_FIRST))
+	{
+		parser->GetAnimation(animated_title, "SegaLogo_Animation");
+		parser->UnloadObject();
+	}
 }
 
 ModuleSceneTitle::~ModuleSceneTitle()
@@ -19,7 +25,11 @@ ModuleSceneTitle::~ModuleSceneTitle()
 
 bool ModuleSceneTitle::Start()
 {
+	bool ret = true;
+
 	LOG("Loading title scene");
+
+	App->renderer->camera.x = App->renderer->camera.y = 0;
 
 	switch (current_state)
 	{
@@ -27,8 +37,17 @@ bool ModuleSceneTitle::Start()
 		if (App->parser->LoadObject(MODULESCENE_FIRST))
 		{
 			texture = App->textures->Load(App->parser->GetString("TexturePath"));
-			App->parser->UnloadObject();
+			ffade_time = abs(App->parser->GetFloat("FadeTime"));
+			float seconds = abs(App->parser->GetFloat("TimeSeconds"));
+			ret = App->parser->UnloadObject();
+
+			seconds -= ffade_time;
+			seconds = (seconds > 0.0f) ? seconds : ffade_time;
+			timer.Start((Uint32)seconds * 1000);
 		}
+		else
+			ret = false;
+		App->audio->PlayMusic("assets/mus_rosetta-stone.ogg");
 		break;
 	case LOGO2:
 		if (App->parser->LoadObject(MODULESCENE_SECOND))
@@ -39,16 +58,56 @@ bool ModuleSceneTitle::Start()
 			backcolor_g = App->parser->GetValueFromArray(1);
 			backcolor_b = App->parser->GetValueFromArray(2);
 			backcolor_a = App->parser->GetValueFromArray(3);
-			App->parser->UnloadObject();
+			ffade_time = abs(App->parser->GetFloat("FadeTime"));
+			float seconds = abs(App->parser->GetFloat("TimeSeconds"));
+			ret = App->parser->UnloadObject();
+
+			seconds -= ffade_time;
+			seconds = (seconds > 0.0f) ? seconds : ffade_time;
+			timer.Start((Uint32)seconds * 1000);
 		}
+		else
+			ret = false;
+		break;
+	case TITLE_SCROLL:
+		if (App->parser->LoadObject(MODULESCENE_THIRD))
+		{
+			texture = App->textures->Load(App->parser->GetString("TexturePath"));
+			App->parser->GetRect(background, "BackgroundRect");
+			App->parser->GetRect(title, "TitleRect");
+			int speed = abs(App->parser->GetInt("TitleSpeed"));
+			fspeed_title = -(float)speed;
+			ispeed_camera = abs(App->parser->GetInt("CameraSpeed")) * App->window->GetScreenSize();
+			ffade_time = abs(App->parser->GetFloat("FadeTime"));
+			ret = App->parser->UnloadObject();
+
+			ifinalpos_cam = App->window->GetScreenWidth() - background.w;
+			ipos_background = { 0,0 };
+			ipos_title = { (ifinalpos_cam * speed + (App->window->GetScreenWidth() - title.w) / 2), 0 };
+			ifinalpos_cam *= App->window->GetScreenSize();
+		}
+		else
+			ret = false;
 		break;
 	case TITLE:
+		if (App->parser->LoadObject(MODULESCENE_THIRD))
+		{
+			texture = App->textures->Load(App->parser->GetString("TexturePath"));
+			App->parser->GetRect(background, "BackgroundRect");
+			App->parser->GetRect(title, "TitleRect");
+			App->parser->GetRect(subtitle, "SubtitleRect");
+			ret = App->parser->UnloadObject();
+
+			ipos_background = { App->window->GetScreenWidth() - background.w,0 };
+			ipos_title = { (App->window->GetScreenWidth() - title.w) / 2,0 };
+			ipos_subtitle = { (App->window->GetScreenWidth() - title.w) / 2,title.h };
+		}
+		else
+			ret = false;
 		break;
 	default:
 		break;
 	}
-
-	App->renderer->camera.x = App->renderer->camera.y = 0;
 
 	return true;
 }
@@ -60,16 +119,18 @@ bool ModuleSceneTitle::CleanUp()
 	switch (current_state)
 	{
 	case LOGO1:
-		previous_screen = LOGO1;
+		timer.Stop();
 		current_state = LOGO2;
 		break;
 	case LOGO2:
-		previous_screen = LOGO2;
-		current_state = LOGO1;
+		timer.Stop();
+		current_state = TITLE_SCROLL;
+		break;
+	case TITLE_SCROLL:
+		current_state = TITLE;
 		break;
 	case TITLE:
-		previous_screen = TITLE;
-		current_state = UNKNOWN;
+		current_state = TITLE_SCROLL;
 		break;
 	}
 
@@ -84,14 +145,29 @@ update_status ModuleSceneTitle::Update()
 	{
 	case LOGO1:
 		App->renderer->BlitCentered(texture, &(animated_title.GetCurrentFrame()));
-		if (App->input->GetKey(SDL_SCANCODE_RETURN) == KEY_DOWN && App->fade->isFading() == false)
-			App->fade->FadeToBlack(this, this);
+		if (timer.MaxTimeReached() == true && App->fade->isFading() == false)
+			App->fade->FadeToBlack(this, this, ffade_time);
 		break;
 	case LOGO2:
 		App->renderer->DrawQuad(App->renderer->camera, backcolor_r, backcolor_g, backcolor_b, backcolor_a, false);
 		App->renderer->BlitCentered(texture, NULL);
+		if ((App->input->GetKey(SDL_SCANCODE_RETURN) == KEY_DOWN || timer.MaxTimeReached() == true) && App->fade->isFading() == false)
+			App->fade->FadeToBlack(this, this, ffade_time);
+		break;
+	case TITLE_SCROLL:
+		if (App->renderer->camera.x > ifinalpos_cam)
+			App->renderer->camera.x -= ispeed_camera;
+		App->renderer->Blit(texture, ipos_background, &background);
+		App->renderer->Blit(texture, ipos_title, &title, fspeed_title);
+		if (App->renderer->camera.x <= ifinalpos_cam && App->fade->isFading() == false)
+			App->fade->FadeToBlack(this, this, ffade_time);
 		break;
 	case TITLE:
+		App->renderer->Blit(texture, ipos_background, &background);
+		App->renderer->Blit(texture, ipos_title, &title);
+		App->renderer->Blit(texture, ipos_subtitle, &subtitle);
+		if (App->input->GetKey(SDL_SCANCODE_RETURN) == KEY_DOWN && App->fade->isFading() == false)
+			App->fade->FadeToBlack(this, this);
 		break;
 	default:
 		break;
--- ModuleSceneTitle.h
+++ ModuleSceneTitle.h
@@ -3,11 +3,13 @@
 
 #include "Module.h"
 #include "Animation.h"
+#include "Point.h"
+#include "Timer.h"
 
 #define MODULESCENE_TITLE "ModuleScene_Title"
 #define MODULESCENE_FIRST "Config.Scenes.Title.FirstScreen"
-#define MODULESCENE_FIRST_ANIMATION "Config.Scenes.Title.FirstScreen.SegaLogo_Animation"
 #define MODULESCENE_SECOND "Config.Scenes.Title.SecondScreen"
+#define MODULESCENE_THIRD "Config.Scenes.Title.TitleScreen"
 
 class SDL_Texture;
 class JSONParser;
@@ -17,6 +19,7 @@ enum TitleScreen
 	UNKNOWN,
 	LOGO1,
 	LOGO2,
+	TITLE_SCROLL,
 	TITLE
 };
 
@@ -34,14 +37,27 @@ private:
 	SDL_Texture* texture = nullptr;
 
 	Animation animated_title;
+	SDL_Rect background = { 0,0,0,0 };
+	SDL_Rect title = { 0,0,0,0 };
+	SDL_Rect subtitle = { 0,0,0,0 };
+
+	iPoint ipos_background = { 0,0 };
+	iPoint ipos_title = { 0,0 };
+	iPoint ipos_subtitle = { 0,0 };
+
+	float fspeed_title = 1.0f;
+	int ispeed_camera = 1;
+	int ifinalpos_cam = 0;
 
 	Uint8 backcolor_r = 0;
 	Uint8 backcolor_g = 0;
 	Uint8 backcolor_b = 0;
 	Uint8 backcolor_a = 0;
 
-	TitleScreen current_state = LOGO1;
-	TitleScreen previous_screen = UNKNOWN;
+	TitleScreen current_state = UNKNOWN;
+
+	Timer timer;
+	float ffade_time;
 };
 
 
